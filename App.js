51665b56baf9779f3c20a9ffe85e70d2d0b54eba
masudha.meher@telekom-digital.com <masudha.meher@telekom-digital.com[alias]>
2024-10-02 21:43:45 +0530

    namaste-react-starter-pack

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {
+    id: 'heading'
+}, "Hello World From React from a diff file");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
