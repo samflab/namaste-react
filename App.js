41ce797bb4809e585dbcba1941cd6826e568fe89
masudha.meher@telekom-digital.com <masudha.meher@telekom-digital.com[alias]>
2024-10-04 20:51:17 +0530

    add: html-using-create Element-React

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-const heading = React.createElement("h1", {
-    id: 'heading'
-}, "Hello World From React from a diff file");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, "Child 1"),
+  React.createElement("div", { id: "child2" }, "Child 2"),
+  React.createElement("div", { id: "child3" }, [
+    React.createElement("div", { id: "grandchild1" }, "Grandchild 1"),
+    React.createElement("div", { id: "grandchild2" }, "Grandchild 2"),
+  ]),
+]);
+
+console.log("parent", parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
